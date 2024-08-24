2a53e0fcf4ddf744ec75ab8866550b4c0358eca1
Zhuo Chen <36575683+JoeChen-YT@users.noreply.github.com>
2024-08-24 10:59:28 -0700

    P1 Project for Guess Number Game - Udemy JavaScript Course

--- p1-guess-my-number/script.js
+++ p1-guess-my-number/script.js
@@ -1,10 +1,27 @@
 "use strict";
 
-let score = 20;
-const secreteNumber = Math.trunc(Math.random() * 20 + 1);
+const initialScore = 20;
+const initialNumberTextContent = "?";
+const initialMessageTextContext = "Start guessing...";
+const initialGuessInputFieldTextContext = "";
 
-// TODO: Remove this line after finish the code
-document.querySelector(".number").textContent = secreteNumber;
+let highestScore = 0;
+let score = initialScore;
+let secreteNumber = Math.trunc(Math.random() * 20 + 1);
+
+document.querySelector(".again").addEventListener("click", function () {
+  score = initialScore;
+  secreteNumber = Math.trunc(Math.random() * 20 + 1);
+
+  document.querySelector(".message").textContent = initialMessageTextContext;
+  document.querySelector(".number").textContent = initialNumberTextContent;
+  document.querySelector(".score").textContent = initialScore;
+  document.querySelector(".guess").value = initialGuessInputFieldTextContext;
+
+  document.querySelector("body").style.backgroundColor = "#222";
+  document.querySelector(".number").style.width = "15rem";
+  document.querySelector(".check").style.visibility = "visible";
+});
 
 // Add a click event listener on "Check Button"
 document.querySelector(".check").addEventListener("click", function () {
@@ -17,7 +34,17 @@ document.querySelector(".check").addEventListener("click", function () {
   } else {
     if (score > 0) {
       if (guess === secreteNumber) {
+        document.querySelector("body").style.backgroundColor = "#60b347";
+        const numberElement = document.querySelector(".number");
+        numberElement.style.width = "30rem";
+        numberElement.textContent = secreteNumber;
         messageElement.textContent = "🎉 You got it";
+
+        if (score > highestScore) {
+          highestScore = score;
+          document.querySelector(".highscore").textContent = highestScore;
+          document.querySelector(".check").style.visibility = "hidden";
+        }
       } else {
         score--;
         const scoreElement = (document.querySelector(".score").textContent =
